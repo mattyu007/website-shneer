d07efa056e24a5d096fe827b8b7bcc9a2c767a42
Matthew Yu <mattyu007@gmail.com>
2016-04-02 04:58:06 -0400

    Added link back to main site and improved shnEEEeEeeeEEer
    - Add footer link back to main site
    - Randomize size of added 'e's and randomly capitalize the 'e's

--- js/shneer.js
+++ js/shneer.js
@@ -1,9 +1,14 @@
 'use strict';
 
-var shneer = document.getElementById('shneer');
-
 window.addEventListener('load', function() {
+    var shneer = document.getElementById('shneer');
+    var r = document.getElementById('r');
+
     setInterval(function() {
-        shneer.innerText = shneer.innerText.replace("er", "eer");
+        var e = document.createElement('span');
+        e.style.fontSize = (Math.random() * 100 + 50) + '%';
+        e.innerText = Math.random() < 0.85 ? 'e' : 'E';
+        
+        shneer.insertBefore(e, r);
     }, 20);
 });
