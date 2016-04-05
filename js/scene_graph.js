76e8cbde056b3030e190ebd521bc82d85bc3cdf0
Matthew Yu <mattyu007@gmail.com>
2016-04-04 23:24:15 -0400

    Added basic scene graph, affine transform lib

---
+++ js/scene_graph.js
@@ -0,0 +1,270 @@
+'use strict';
+
+/**
+ * Create and return the scene graph classes.
+ * 
+ * Based on scene_graph.js from cs349-specia, scaffolded by the course IAs.
+ */
+function createSceneGraph() {
+    
+    /**
+     * The abstract base scene graph node.
+     * 
+     * @param {string} id a node identifier
+     * @param {GraphNode?} parent the parent node
+     */
+    var GraphNode = function(id, parent) {
+        // An identifier for this node.
+        this.id = id;
+        
+        // The local transformation (relative to the parent) and
+        // the global transformation (relative to the canvas).
+        this.localTransformation = new AffineTransform();
+        this.globalTransformation = new AffineTransform();
+        
+        // If a valid parent was passed in, save the parent, and add this
+        // node as a child of the parent.
+        this.parent = parent ? parent : null;
+        if (this.parent) {
+            this.parent.addChild(this);
+        }
+        
+        // A list of child nodes.
+        this.children = [];
+        
+        // The local bounding box of this node, in the local coordinate space.
+        // Should be overridden by concrete child nodes.
+        this.localBoundingBox = {
+            x: 0,
+            y: 0,
+            w: 0,
+            h: 0
+        };
+        
+        // A list of listeners.
+        this.listeners = [];
+    }
+    
+    _.extend(GraphNode.prototype, {
+        /**
+         * Notify all listeners to update due to a change in this node.
+         */
+        notify: function() {
+            _.each(this.listeners, function(listener) {
+                listener.update();
+            });
+        },
+        
+        /**
+         * Add a listener, if it hasn't already been added.
+         * 
+         * @param listener the listener to add
+         */
+        addListener: function(listener) {
+            if (!_.contains(this.listeners, listener)) {
+                this.listeners.push(listener);
+            }
+        },
+        
+        /**
+         * Remove a listener, if it was previously added to this node.
+         * 
+         * @param listener the listener to remove
+         */
+        removeListener: function(listener) {
+            this.listeners = _.without(this.listeners, listener);
+        },
+        
+        /**
+         * Add a child to this node, if it hasn't already been added.
+         * 
+         * Triggers an update of the global transformations in the node being added.
+         * 
+         * @param node the node to add
+         */
+        addChild: function(node) {
+            if (!_.contains(this.children, node)) {
+                this.children.push(node);
+                
+                // Set this node to be the child's parent
+                node.parent = this;
+                
+                // Update the global transformations of the new child
+                node.updateAllGlobalTransformations();
+            }
+        },
+        
+        /**
+         * Remove a child from this node, if it was previously added to this node.
+         * 
+         * @param node the node to remove
+         */
+        removeChild: function(node) {
+            this.children = _.without(this.children, node);
+        },
+        
+        /**
+         * Apply an AffineTransform to the Canvas context.
+         * 
+         * @param context the canvas context
+         * @param transformation the affine transform
+         */
+        applyTransformationToContext: function(context, transformation) {
+            context.transform(transformation.m00_, 
+                transformation.m10_,
+                transformation.m01_,
+                transformation.m11_,
+                transformation.m02_,
+                transformation.m12_);
+        },
+        
+        /**
+         * Update the global transformation of this node only.
+         */
+        updateGlobalTransformation: function() {
+            // If this is the root of the scene graph
+            if (!this.parent) {
+                // Update the global transformation with the local transformation
+                this.globalTransformation.copyFrom(this.localTransformation);
+            }
+
+            // Otherwise (if this is not the root of the scene graph)
+            else {
+                // Clone the global transformation of the parent
+                var parentGlobalTransformationClone = this.parent.globalTransformation.clone();
+
+                // Concatenate it with this node's local transformation
+                parentGlobalTransformationClone.concatenate(this.localTransformation);
+                this.globalTransformation.copyFrom(parentGlobalTransformationClone);
+            }
+        },
+        
+        /**
+         * Recursively update the global transformation of this node and those
+         * of its children.
+         */
+        updateAllGlobalTransformations: function() {
+            // Update this node's global transformation
+            this.updateGlobalTransformation();
+            
+            // Update each child node's global transformations recursively
+            _.each(this.children, function(node) {
+                node.updateAllGlobalTransformations();
+            });
+        },
+        
+        /**
+         * Render this node only.
+         * 
+         * @param context the Canvas context
+         */
+        renderLocal: function(context) {
+            // Base implementation does nothing; concrete nodes should override
+            // this and render themselves. 
+        },
+        
+        /**
+         * Recursively render this node and its children.
+         * 
+         * @param context the Canvas context
+         */
+        renderAll: function(context) {
+            context.save();
+            
+            // Apply the local transformation
+            this.applyTransformationToContext(context, this.localTransformation);
+            
+            // Render this node
+            this.renderLocal(context);
+            
+            // Render each child recursively
+            _.each(this.children, function(child) {
+                child.renderAll(context);
+            })
+            
+            context.restore();
+        },
+        
+        /**
+         * Rotate this node and its children.
+         * 
+         * @param theta angle of rotation clockwise (in radians)
+         * @param x x-coordinate of the centre of rotation
+         * @param y y-coordinate of the centre of rotation
+         */
+        rotate: function(theta, x, y) {
+            // Rotate the local transformation
+            this.localTransformation.rotate(theta, x, y);
+            
+            // Recursively update the global transformations
+            this.updateAllGlobalTransformations();
+            
+            // Notify listeners about the change
+            this.notify();
+        },
+        
+        /**
+         * Translate this node and its children.
+         * 
+         * @param dx amount to translate horizontally in the local coordinate space
+         * @param dy amount to translate vertically in the local coordinate space
+         */
+        translate: function(dx, dy) {
+            // Translate the local transformation
+            this.localTransformation.translate(dx, dy);
+            
+            // Recursively update the global transformations
+            this.updateAllGlobalTransformations();
+            
+            // Notify listeners about the change
+            this.notify();
+        },
+        
+        /**
+         * Translate this node and its children with respect to the global coordinate space.
+         * 
+         * @param dx amount to translate horizontally in the global coordinate space
+         * @param dy amount to translate vertically in the global coordinate space
+         */
+        translateAsGlobal: function(dx, dy) {
+            // Transform the global deltas so that they are relative to the local
+            // coordinate space
+            var transformedPoints = [];
+            this.globalTransformation.createInverse().transform(
+                /* src    */ [0, 0, dx, dy],
+                /* srcOff */ 0,
+                /* dst    */ transformedPoints, 
+                /* dstOff */ 0,
+                /* numPts */ 2
+            );
+            
+            // Calculate the delta in the local coordinate space
+            var deltaX = transformedPoints[2] - transformedPoints[0];
+            var deltaY = transformedPoints[3] - transformedPoints[1];
+            
+            // Translate in the local coordinate space
+            this.translate(deltaX, deltaY);
+        },
+        
+        /**
+         * Scale this node and its children.
+         * 
+         * @param sy horizontal scaling factor
+         * @param sy vertical scaling factor
+         */
+        scale: function(sx, sy) {
+            // Scale the local transformation
+            this.localTransformation.scale(sx, sy);
+            
+            // Recursively update the global transformations
+            this.updateAllGlobalTransformations();
+            
+            // Notify listeners about the change
+            this.notify();
+        }
+    });
+    
+    return {
+        GraphNode: GraphNode
+    }
+}
